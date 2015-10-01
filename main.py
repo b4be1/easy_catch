bbc2f2ebaed030985623a71d66558181fd5f3ec5
Boris Belousov <b.a.belousov@gmail.com>
2015-10-01 10:16:18 +0200

    can simulate and plot a simple trajectory

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ np.set_printoptions(suppress=True, precision=4)
 __author__ = 'belousov'
 
 
-# ---- Create model ---- #
+# ============================================================================
+#                              Initialization
+# ============================================================================
+# ----------------------------- Create model ------------------------------- #
 # Initial state
 x0 = ca.DMatrix([0, 0, 0, 5, 5, 10, 5, 0])
 # Final cost of coordinate discrepancy
@@ -26,33 +29,40 @@ R = ca.diagcat([1, 0])
 dt = 0.1
 model = Model(x0, (w_cl, R), dt)
 
-# ---- Create simulator ---- #
+# -------------------------- Create simulator ------------------------------ #
 # Time horizon
-l = 10
+l = 20
 # Nominal controls for simulation
 u_all = model.u.repeated(ca.DMatrix.zeros(model.nu, l))
 u_all[:, 'v'] = 2
 u_all[:, 'phi'] = ca.pi/3
 simulator = Simulator(model, l, u_all)
 
-# ---- Create plotter ---- #
+# ---------------------------- Create plotter ------------------------------ #
 plotter = Plotter()
 
+
+# ============================================================================
+#                           Simulate a trajectory
+# ============================================================================
 # Get a single noise-free trajectory
 x_all = simulator.draw_trajectory()
 
 # Plot trajectory
 fig, ax = plt.subplots(figsize=(6, 6))
-plotter.plot_trajectory('Trajectory', ax, x_all)
+plotter.plot_ball_trajectory('Ball trajectory', ax, x_all)
+plotter.plot_catcher_trajectory('Catcher trajectory', ax, x_all)
+ax.grid(True)
 plt.show()
 
 # Plot trajectory 3D
 fig = plt.figure(figsize=(12, 8))
 ax = fig.add_subplot(111, projection='3d')
-plotter.plot_trajectory('Trajectory 3D', ax, x_all)
+plotter.plot_ball_trajectory_3D('Ball trajectory 3D', ax, x_all)
+plotter.plot_catcher_trajectory_3D('Catcher trajectory 3D', ax, x_all)
+ax.set_zlim(0, 6)
 plt.show()
 
-# todo: trajectory in 3D is not displayed correctly
 
 
 
--- plotter.py
+++ plotter.py
@@ -4,14 +4,32 @@ __author__ = 'belousov'
 class Plotter:
     """Plot simulation results"""
 
-    def plot_trajectory(self, name, ax, x_all):
+    # ------------------------------------------------------------------------
+    #                               2D
+    # ------------------------------------------------------------------------
+    def plot_ball_trajectory(self, name, ax, x_all):
         return ax.plot(x_all[:, 'x_b'],
                        x_all[:, 'y_b'],
                        label=name, lw=0.8, alpha=0.8, color='g',
                        marker='o', markersize=4, fillstyle='none')
 
-    def plot_trajectory_3D(self, name, ax, x_all):
+    def plot_catcher_trajectory(self, name, ax, x_all):
+        return ax.plot(x_all[:, 'x_c'],
+                       x_all[:, 'y_c'],
+                       label=name, lw=0.8, alpha=0.8, color='g',
+                       marker='o', markersize=4, fillstyle='none')
+
+    # ------------------------------------------------------------------------
+    #                               3D
+    # ------------------------------------------------------------------------
+    def plot_ball_trajectory_3D(self, name, ax, x_all):
         return ax.scatter3D(x_all[:, 'x_b'],
                             x_all[:, 'y_b'],
                             x_all[:, 'z_b'],
-                            label=name, c='b', lw='0.1')
+                            label=name, color='g')
+
+    def plot_catcher_trajectory_3D(self, name, ax, x_all):
+        return ax.scatter3D(x_all[:, 'x_c'],
+                            x_all[:, 'y_c'],
+                            0,
+                            label=name, color='g')
